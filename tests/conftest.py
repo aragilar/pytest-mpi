d081a8cbf4071e0e5ef2fefb4f0365a5e29c052a
James Tocknell <aragilar@gmail.com>
2021-03-13 18:11:36 +1100

    Add handling for pytester and testdir

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 from logging import getLogger
+import sys
 
 import py
 import pytest
-from _pytest.pytester import Testdir
 
 log = getLogger(__name__)
 MPI_ARGS = ("mpirun", "-n")
+PYTEST_ARGS = (sys.executable, "-mpytest")
 
 
 @pytest.fixture
@@ -17,41 +18,90 @@ def has_mpi4py():
         return False
 
 
-class MPITestdir(Testdir):
-    def __init__(self, request, tmpdir_factory):
-        super().__init__(request, tmpdir_factory)
-        method = self.request.config.getoption("--runpytest")
+def _to_py_path(p):
+    return py.path.local(p)
+
+
+def _to_pathlib(p):
+    from pathlib import Path
+    return Path(p)
+
+
+class MPITestdir(object):
+    def __init__(self, request, config):
+        method = request.config.getoption("--runpytest")
         if method == "inprocess":
             log.warn("To run the MPI tests, you need to use subprocesses")
+        self._pytester = None
+        self._testdir = None
+        self._setup(request, config)
 
-    def runpytest_subprocess(
-        self, *args, timeout=60, mpi_procs=2, max_retries=5
-    ):
+    def _setup(self, request, config):
         """
-        Based on testdir.runpytest_subprocess
+        This handles the difference between Testdir and PyTester
         """
+        try:
+            self._pytester = request.getfixturevalue("pytester")
+        except:
+            try:
+                self._testdir = request.getfixturevalue("testdir")
+            except:
+                raise RuntimeError(
+                    "Unable to load either pytester or testdir fixtures. "
+                    "Check if pytester plugin is enabled."
+                )
+
+    def makepyfile(self, *args, **kwargs):
+        if self._pytester is not None:
+            self._pytester.makepyfile(*args, **kwargs)
+        else:
+            self._testdir.makepyfile(*args, **kwargs)
+
+    def runpytest(self, *args, **kwargs):
+        if self._pytester is not None:
+            return self._run_pytester(*args, **kwargs)
+        return self._run_testdir(*args, **kwargs)
+
+    def _run_testdir(self, *args, timeout=60, mpi_procs=2, max_retries=5):
         retries = 0
         p = py.path.local.make_numbered_dir(
-            prefix="runpytest-", keep=None, rootdir=self.tmpdir
+            prefix="runpytest-", keep=None, rootdir=self._testdir.tmpdir
         )
         args = ("--basetemp=%s" % p,) + args
-        plugins = [x for x in self.plugins if isinstance(x, str)]
+        plugins = [x for x in self._testdir.plugins if isinstance(x, str)]
         if plugins:
             args = ("-p", plugins[0]) + args
-        args = MPI_ARGS + (str(mpi_procs),) + self._getpytestargs() + args
+        args = MPI_ARGS + (str(mpi_procs),) + PYTEST_ARGS + args
         while retries < max_retries:
             try:
-                return self.run(*args, timeout=timeout)
-            except self.TimeoutExpired as e:
+                return self._testdir.run(*args, timeout=timeout)
+            except self._testdir.TimeoutExpired as e:
                 retries += 1
                 if retries >= max_retries:
                     raise
         raise e
 
-    def runpytest(self, *args, **kwargs):
-        return self.runpytest_subprocess(*args, **kwargs)
+    def _run_pytester(self, *args, timeout=60, mpi_procs=2, max_retries=5):
+        retries = 0
+        p = _to_pathlib(py.path.local.make_numbered_dir(
+            prefix="runpytest-", keep=None,
+            rootdir=_to_py_path(self._pytester.tmpdir)
+        ))
+        args = ("--basetemp=%s" % p,) + args
+        plugins = [x for x in self._pytester.plugins if isinstance(x, str)]
+        if plugins:
+            args = ("-p", plugins[0]) + args
+        args = MPI_ARGS + (str(mpi_procs),) + PYTEST_ARGS + args
+        while retries < max_retries:
+            try:
+                return self._pytester.run(*args, timeout=timeout)
+            except self._pytester.TimeoutExpired as e:
+                retries += 1
+                if retries >= max_retries:
+                    raise
+        raise e
 
 
 @pytest.fixture
-def mpi_testdir(request, tmpdir_factory):
-    return MPITestdir(request, tmpdir_factory)
+def mpi_testdir(request, pytestconfig):
+    return MPITestdir(request, pytestconfig)
